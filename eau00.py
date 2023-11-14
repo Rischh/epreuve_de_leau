5e16bfef68a6db46c3ec1f509655aa2caccd2353
samuel-risch <rischebourg.samuel@gmail.com>
2023-11-14 12:58:02 +0100

    Combinaison de 3 nombres

--- eau00.py
+++ eau00.py
@@ -1,24 +1,10 @@
-def combination_2_numbers(li):
-    li = []
-
-    for first_number in range(100):
-        for second_numbers in range(100):
+def combination_3_numbers():
+    for unit in range(10):
+        for ten in range(10):
+            for hundred in range(10):
             
-            if first_number <= second_numbers:
-                
-                first_number_formate = str(first_number).zfill(2)
-                second_numbers_formate = str(second_numbers).zfill(2)
-                
-                pair = (first_number_formate, second_numbers_formate)
-                li.append(pair)
-    
-    for pair in li:
-        print(pair)
-    
-list = ["%.2d" % i for i in range(100)]
-    
-combination_2_numbers(list)
-    
+                if unit <= ten <= hundred:
+                    print(unit, ten, hundred)
 
-
-
+    
+combination_3_numbers()
