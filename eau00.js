1c12c4b04742cb2516192a773b0b963aaca8a0ba
Rischh <rischebourg.samuel@gmail.com>
2025-03-18 15:42:16 +0100

    feat: eau00 in js

---
+++ eau00.js
@@ -0,0 +1,11 @@
+const combinationsThreeDigits = () => {
+  let result = []
+
+  for (let i = 0; i <= 9; i++)
+    for (let j = i + 1; j <= 9; j++)
+      for (let k = j + 1; k <= 9; k++) result.push(`${i}${j}${k}`)
+
+  return result
+}
+
+console.log(combinationsThreeDigits().join(", "))
