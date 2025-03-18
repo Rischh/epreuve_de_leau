d4f6608e5f09147691aaf16a0aa6eb764dd7d115
Rischh <rischebourg.samuel@gmail.com>
2025-03-18 15:53:10 +0100

    feat: eau01 in js

---
+++ eau01.js
@@ -0,0 +1,11 @@
+const combinationsTwoNumbers = () => {
+  const result = []
+
+  for (let i = 0; i <= 99; i++)
+    for (let j = i + 1; j <= 99; j++)
+      result.push(`${String(i).padStart(2, "0")} ${String(j).padStart(2, 0)}`)
+
+  return result
+}
+
+console.log(combinationsTwoNumbers().join(", "))
