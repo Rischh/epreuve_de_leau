c00418462664bed38ae1e9f6cb5e6aeee00d4ca4
Rischh <rischebourg.samuel@gmail.com>
2025-03-19 10:41:53 +0100

    refactor(eau08): change the name of the resolution function

--- eau08.js
+++ eau08.js
@@ -3,26 +3,33 @@ const isOnlyDigits = (arg) => {
   const NINE_CHAR_CODE = 57
 
   for (let i = 0; i < arg.length; i++) {
-    if (!(arg[i].charCodeAt() >= ZERO_CHAR_CODE && arg[i].charCodeAt() <= NINE_CHAR_CODE)) {
+    if (
+      !(
+        arg[i].charCodeAt() >= ZERO_CHAR_CODE &&
+        arg[i].charCodeAt() <= NINE_CHAR_CODE
+      )
+    ) {
       return false
     }
   }
   return true
 }
 
-const validateArgsCount = (args) => {
+const validateArgCount = (args) => {
   if (args.length !== 1)
-    return console.error("Le programme à besoin d'un unique argument pour fonctionner.")
+    return console.error(
+      "Le programme à besoin d'un unique argument pour fonctionner."
+    )
   return args[0]
 }
 
 const getArgs = () => process.argv.slice(2)
 
-const checkUserInput = () => {
-  const arg = validateArgsCount(getArgs())
+const resolveDigitValidation = () => {
+  const arg = validateArgCount(getArgs())
   if (!arg) return
 
   return isOnlyDigits(arg)
 }
 
-console.log(checkUserInput())
+console.log(resolveDigitValidation())
