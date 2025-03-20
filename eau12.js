9f1a41ff6256681cc245679ded78c5b9dd982496
Rischh <rischebourg.samuel@gmail.com>
2025-03-20 15:39:57 +0100

    refactor(eau12): swap with destructuring

--- eau12.js
+++ eau12.js
@@ -5,9 +5,10 @@ const myBubbleSort = (numbers) => {
     let isSorted = true
     for (let j = 0; j < sortedNumbers.length - i; j++) {
       if (sortedNumbers[j] > sortedNumbers[j + 1]) {
-        const temp = sortedNumbers[j]
-        sortedNumbers[j] = sortedNumbers[j + 1]
-        sortedNumbers[j + 1] = temp
+        ;[sortedNumbers[j], sortedNumbers[j + 1]] = [
+          sortedNumbers[j + 1],
+          sortedNumbers[j],
+        ]
         isSorted = false
       }
     }
@@ -18,31 +19,34 @@ const myBubbleSort = (numbers) => {
 
 const validateArgsCount = (args) => {
   if (args.length < 2)
-    return console.error("Le programme a besoin d'au moins 2 arguments pour fonctionner.")
+    return console.error(
+      "Le programme a besoin d'au moins 2 arguments pour fonctionner."
+    )
   return args
 }
 
-const validateNumericArgs = (args) => {
-  for (let i = 0; i < args.length; i++) {
-    if (isNaN(+args[i]))
-      return console.error(
-        "Le programme a besoin d'une liste composée uniquement de nombres pour fonctionner."
-      )
-    args[i] = Number(args[i])
-  }
-  return args
+const validateNumericArg = (n) => {
+  if (isNaN(n))
+    return console.error(
+      "Le programme a besoin d'une liste de nombres entiers pour fonctionner."
+    )
+  return true
 }
 
 const getArguments = () => process.argv.slice(2)
 
-const resolveBubbleSort = () => {
+const displayBubbleSort = () => {
   const args = validateArgsCount(getArguments())
   if (!args) return
 
-  const numbers = validateNumericArgs(args)
-  if (!numbers) return
+  const numbers = [...args]
 
-  return myBubbleSort(numbers)
+  for (let i = 0; i < numbers.length; i++) {
+    if (!validateNumericArg(numbers[i])) return
+    numbers[i] = +numbers[i]
+  }
+
+  return myBubbleSort(numbers).join(" ")
 }
 
-console.log(resolveBubbleSort())
+console.log(displayBubbleSort())
