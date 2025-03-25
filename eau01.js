88d6baf6408215cb20bfa4094dc3a9fa5503663d
Rischh <rischebourg.samuel@gmail.com>
2025-03-25 16:45:45 +0100

    refactor(eau00 & 01): better format for the main function

--- eau01.js
+++ eau01.js
@@ -1,11 +1,13 @@
-const combinationsTwoNumbers = () => {
+const getCombinationsTwoNumbers = () => {
   const result = []
 
-  for (let i = 0; i <= 99; i++)
-    for (let j = i + 1; j <= 99; j++)
+  for (let i = 0; i <= 99; i++) {
+    for (let j = i + 1; j <= 99; j++) {
       result.push(`${String(i).padStart(2, "0")} ${String(j).padStart(2, 0)}`)
+    }
+  }
 
   return result
 }
 
-console.log(combinationsTwoNumbers().join(", "))
+console.log(getCombinationsTwoNumbers().join(", "))
