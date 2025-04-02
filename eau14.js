599e0cb774f5b7a696e3925d06562467cf62466f
Rischh <rischebourg.samuel@gmail.com>
2025-04-02 15:49:52 +0200

    refactor(eau14): sort but take into consideration same char

--- eau14.js
+++ eau14.js
@@ -1,24 +1,55 @@
-const getSortedByAscii = (values) => {
-  const sortedValues = [...values]
+const getSortedByAscii = (array) => {
+  const charCodes = getCharCodes(array)
+  const sortedArray = getBubbleSort(charCodes)
 
-  for (let i = 0; i < sortedValues.length; i++) {
+  const result = []
+  const arrayClone = [...array]
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < arrayClone.length; j++) {
+      if (arrayClone[j].charCodeAt() === sortedArray[i]) {
+        result.push(arrayClone[j])
+        arrayClone.splice(j, 1)
+        break
+      }
+    }
+  }
+
+  return result
+}
+
+const getCharCodes = (array) => {
+  const charCodes = []
+
+  for (let i = 0; i < array.length; i++) {
+    charCodes.push(array[i].charCodeAt(0))
+  }
+
+  return charCodes
+}
+
+const getBubbleSort = (numbers) => {
+  const sortedNumbers = [...numbers]
+
+  for (let i = 0; i < sortedNumbers.length; i++) {
     let isSorted = true
-    for (let j = 0; j < sortedValues.length - 1; j++) {
-      if (sortedValues[j].charCodeAt() > sortedValues[j + 1].charCodeAt()) {
-        ;[sortedValues[j], sortedValues[j + 1]] = [
-          sortedValues[j + 1],
-          sortedValues[j],
+    for (let j = 0; j < sortedNumbers.length - i; j++) {
+      if (sortedNumbers[j] > sortedNumbers[j + 1]) {
+        ;[sortedNumbers[j], sortedNumbers[j + 1]] = [
+          sortedNumbers[j + 1],
+          sortedNumbers[j],
         ]
         isSorted = false
       }
     }
-    if (isSorted) return sortedValues
+
+    if (isSorted) return sortedNumbers
   }
 
-  return sortedValues
+  return sortedNumbers
 }
 
-const validateArgsCount = (args) => {
+const isValidArgsLength = (args) => {
   if (args.length < 2)
     return console.error(
       "Le programme a besoin d'au moins 2 arguments pour fonctionner."
@@ -32,10 +63,10 @@ const getArgs = () => {
 }
 
 const resolveSortedByAscii = () => {
-  const args = validateArgsCount(getArgs())
+  const args = isValidArgsLength(getArgs())
   if (!args) return
 
-  return getSortedByAscii(args).join(" ")
+  return getSortedByAscii(args)
 }
 
 console.log(resolveSortedByAscii())
